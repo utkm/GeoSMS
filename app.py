24177d7b938db9cf347493b76d57e2c2c93574c7
Ali Syed <77256651+smartahsan@users.noreply.github.com>
2021-09-19 02:43:33 -0400

    updated app.py with new invokes

--- app.py
+++ app.py
@@ -1,37 +1,174 @@
+from flask import Flask
+from flask import request
 import json
-from flask import Flask, request
 from twilio import twiml
-from twilio.twiml.messaging_response import MessagingResponse
+from twilio.twiml.messaging_response import Body, Message, MessagingResponse
+import requests
+#system libraries
+import os
+import asyncio
+import random
+from random import choice
+from random import randrange
+import time
+from types import CoroutineType
+import pylint_ignore
+import pause, datetime
+import pause
+from time import sleep
+import threading
+import signal
+import sys
+from sys import exit
 
-app = Flask(__name__)
 
-# User defined variables
-number = request.form['From']
-message_body = request.form['Body']
+
+
+
+# Playwright Imports 
+from playwright.sync_api import sync_playwright
+
+#recaptcha libraries
+import requests
+
+
+# Early link gen Libraries
+import requests
+import numpy
+import json
+
+# License libraries
+
+
+# Discord Libraries
+from pypresence import Presence
+import json
+
+##############################
+
+
+app = Flask(__name__) 
 
 @app.route('/')
 def home():
     return "Hello"
 
-
-@app.route('/transitapi', methods = ['POST', 'GET'])
+@app.route('/transitapi',methods = ['POST'])
 def transitprocess():
     jsondata = request.get_json()
     data = json.loads(jsondata)
 
     if data['BUSID'] == '123':
-        result = {
-            'Bus Stop':'example: 15 Minutes Away'
+
+        result = {'Bus Stop':'15 Minutes Away'
         }
         return json.dumps(result)
 
-
 @app.route('/sms', methods=['POST'])
 def sms():
-    resp = MessagingResponse()
-    resp.message('Hello {}, you said: {}'.format(number, message_body))
-    return str(resp)
+    number = request.form['From']
+    message_body = request.form['Body']
+
+    my_list = message_body.split(",")
+
+    
+    while True:
+        try:
+            if my_list[0] == 'directions':
+                address = my_list[1]
+                address2 = my_list[2]
+
+                #To get long and lat for first addy
+                url = "https://api.geoapify.com/v1/geocode/autocomplete?text="+address+"&type=street&apiKey=5740005f049146be9b4392e52c07fec3"
+
+                payload={}
+                headers = {}
+
+                response = requests.request("GET", url, headers=headers, data=payload)
+
+                print(response.text)
+
+                j = response.text
+
+                jsonStr = json.loads(j)
+
+                long = jsonStr['features'][0]['properties']['lon']
+
+                print(long)
+
+                lat = jsonStr['features'][0]['properties']['lat']
+
+                print(lat)
+
+
+                #To get long and lat for second addy
+                url2 = "https://api.geoapify.com/v1/geocode/autocomplete?text="+address2+"&type=street&apiKey=5740005f049146be9b4392e52c07fec3"
+
+                payload={}
+                headers = {}
+
+                response2 = requests.request("GET", url2, headers=headers, data=payload)
+
+                print(response2.text)
+
+                j2 = response2.text
+
+                jsonStr2 = json.loads(j2)
+
+                long2 = jsonStr2['features'][0]['properties']['lon']
+
+                print(long2)
+
+                lat2 = jsonStr2['features'][0]['properties']['lat']
+
+                print(lat2)
+
+
+                url = "https://api.geoapify.com/v1/routing?waypoints="+str(lat)+","+str(long)+"|"+str(lat2)+","+str(long2)+"&mode=walk&lang=en&apiKey=5740005f049146be9b4392e52c07fec3"
+
+                payload={}
+                headers = {}
+
+                response = requests.request("GET", url, headers=headers, data=payload)
+
+                d = response.text
+
+                stepsjson = json.loads(d)
+
+
+                instructions = stepsjson['features'][0]['properties']['legs'][0]['steps']
+
+
+                print('###########################')
+
+                directions = []
+
+                for item in instructions:
+                    x = item['instruction']['text']
+                    y = item['distance']
+                    z = str(x) + " - " + "Distance " + str(y) + " Metres"
+                    directions.append(z)
+
+                removesqbrackl = str(directions).replace("[", "")
+
+                removesqbrackr = removesqbrackl.replace("]", "")
+
+                removeqs = removesqbrackr.replace("'", "\n")
+
+                removecomma = removeqs.replace(",", "")
+
+
+                print(removecomma)
+
+                resp = MessagingResponse()
+                resp.message(removecomma)
+                
+                return str(resp)
+            break
+        except:
+            break 
+
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=80, debug=True)
