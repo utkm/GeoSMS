3c451934261d08ae7f3c38e1b382f5f54fc73881
Utkarsh M <m.utkarsh10@gmail.com>
2021-09-19 04:02:34 -0400

    connect functions to server

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from time import sleep
 import threading
 import signal
 import requests
-
+from features import *
 ##############################
 
 
@@ -19,122 +19,34 @@ app = Flask(__name__)
 def home():
     return "Hello"
 
-@app.route('/transitapi',methods = ['POST'])
-def transitprocess():
-    jsondata = request.get_json()
-    data = json.loads(jsondata)
-
-    if data['BUSID'] == '123':
-
-        result = {'Bus Stop':'15 Minutes Away'
-        }
-        return json.dumps(result)
 
 @app.route('/sms', methods=['POST'])
 def sms():
     number = request.form['From']
     message_body = request.form['Body']
+    message_body = message_body.split(',')
+    resp = MessagingResponse()
+    key_command = message_body[0]
+    user_parameter = message_body[1]
 
-    my_list = message_body.split(",")
-
-    
-    while True:
-        try:
-            if my_list[0] == 'directions':
-                address = my_list[1]
-                address2 = my_list[2]
-
-                #To get long and lat for first addy
-                url = "https://api.geoapify.com/v1/geocode/autocomplete?text="+address+"&type=street&apiKey=5740005f049146be9b4392e52c07fec3"
-
-                payload={}
-                headers = {}
-
-                response = requests.request("GET", url, headers=headers, data=payload)
-
-                print(response.text)
-
-                j = response.text
-
-                jsonStr = json.loads(j)
-
-                long = jsonStr['features'][0]['properties']['lon']
-
-                print(long)
-
-                lat = jsonStr['features'][0]['properties']['lat']
-
-                print(lat)
-
-
-                #To get long and lat for second addy
-                url2 = "https://api.geoapify.com/v1/geocode/autocomplete?text="+address2+"&type=street&apiKey=5740005f049146be9b4392e52c07fec3"
-
-                payload={}
-                headers = {}
-
-                response2 = requests.request("GET", url2, headers=headers, data=payload)
-
-                print(response2.text)
-
-                j2 = response2.text
-
-                jsonStr2 = json.loads(j2)
-
-                long2 = jsonStr2['features'][0]['properties']['lon']
-
-                print(long2)
-
-                lat2 = jsonStr2['features'][0]['properties']['lat']
-
-                print(lat2)
-
-
-                url = "https://api.geoapify.com/v1/routing?waypoints="+str(lat)+","+str(long)+"|"+str(lat2)+","+str(long2)+"&mode=walk&lang=en&apiKey=5740005f049146be9b4392e52c07fec3"
-
-                payload={}
-                headers = {}
-
-                response = requests.request("GET", url, headers=headers, data=payload)
-
-                d = response.text
-
-                stepsjson = json.loads(d)
-
-
-                instructions = stepsjson['features'][0]['properties']['legs'][0]['steps']
-
-
-                print('###########################')
-
-                directions = []
-
-                for item in instructions:
-                    x = item['instruction']['text']
-                    y = item['distance']
-                    z = str(x) + " - " + "Distance " + str(y) + " Metres"
-                    directions.append(z)
-
-                removesqbrackl = str(directions).replace("[", "")
-
-                removesqbrackr = removesqbrackl.replace("]", "")
-
-                removeqs = removesqbrackr.replace("'", "\n")
-
-                removecomma = removeqs.replace(",", "")
-
-
-                print(removecomma)
-
-                resp = MessagingResponse()
-                resp.message(removecomma)
-                
-                return str(resp)
-            break
-        except:
-            break 
-
-
+    if key_command == "define":
+        resp.message(get_definition(user_parameter))
+        return str(resp)
+    elif key_command == "advice":
+        resp.message(random_advice())
+        return str(resp)
+    elif key_command == "joke":
+        resp.message(give_joke())
+        return str(resp)
+    elif key_command == "search":
+        resp.message(search_google(user_parameter))
+        return str(resp)
+    elif key_command == "crypto":
+        resp.message(crypto(user_parameter))
+        return str(resp)
+    elif key_command == "directions":
+        resp.message(directions(user_parameter, message_body[2]))
+        return str(resp)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
--- features.py
+++ features.py
@@ -1,6 +1,6 @@
 import requests, json
 
-# format: define (word)
+# format: define, (word)
 def get_definition(word):
     base_url = "https://api.dictionaryapi.dev/api/v2/entries/en/"
     parent = requests.get(base_url+word).json()
@@ -9,7 +9,7 @@ def get_definition(word):
 #print(get_definition("Salubrious"))
 
 
-# format: random advice
+# format: advice
 def random_advice():
     base_url = "https://api.adviceslip.com/advice"
     parent = requests.get(base_url).json()
@@ -52,3 +52,111 @@ def search_google(query):
     answer = answer[:1]
     return answer[0]
 #print(search_google("do sharks eat humans?"))
+
+# crypto,eth
+def crypto(cryptoname):
+    name = cryptoname
+    realcrp = name.upper()
+    url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol="+realcrp+""
+    payload={}
+    headers = {
+    'X-CMC_PRO_API_KEY': '040e1fec-997b-4f47-aecc-8be3ec15760c'
+    }
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+
+    crp = response.text
+
+    stepsjson2 = json.loads(crp)
+
+    price = stepsjson2['data'][realcrp]['quote']['USD']['price']
+
+    p24change = stepsjson2['data'][realcrp]['quote']['USD']['percent_change_24h']
+
+    finalret = "The Current Value Of "+str(realcrp)+" Is "+str(price)+" Dollars And The Percent 24 Hour Change Is "+str(p24change)+""
+
+    return str(finalret)
+
+# directions,address1,address2
+def directions(address, address2):
+    #To get long and lat for first addy
+    url = "https://api.geoapify.com/v1/geocode/autocomplete?text="+address+"&type=street&apiKey=5740005f049146be9b4392e52c07fec3"
+
+    payload={}
+    headers = {}  
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+
+    #print(response.text)
+
+    j = response.text
+
+    jsonStr = json.loads(j)
+
+    long = jsonStr['features'][0]['properties']['lon']
+
+    #print(long)
+
+    lat = jsonStr['features'][0]['properties']['lat']
+
+    #print(lat)
+
+
+    #To get long and lat for second addy
+    url2 = "https://api.geoapify.com/v1/geocode/autocomplete?text="+address2+"&type=street&apiKey=5740005f049146be9b4392e52c07fec3"
+
+    payload={}
+    headers = {}
+
+    response2 = requests.request("GET", url2, headers=headers, data=payload)
+
+    #print(response2.text)
+
+    j2 = response2.text
+
+    jsonStr2 = json.loads(j2)
+
+    long2 = jsonStr2['features'][0]['properties']['lon']
+
+    #print(long2)
+
+    lat2 = jsonStr2['features'][0]['properties']['lat']
+
+    #print(lat2)
+
+
+    url = "https://api.geoapify.com/v1/routing?waypoints="+str(lat)+","+str(long)+"|"+str(lat2)+","+str(long2)+"&mode=walk&lang=en&apiKey=5740005f049146be9b4392e52c07fec3"
+
+    payload={}
+    headers = {}
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+
+    d = response.text
+
+    stepsjson = json.loads(d)
+
+
+    instructions = stepsjson['features'][0]['properties']['legs'][0]['steps']
+
+    print('###########################')
+
+    directions = []
+
+    for item in instructions:
+        x = item['instruction']['text']
+        y = item['distance']
+        z = str(x) + " - " + "Distance " + str(y) + " Metres"
+        directions.append(z)
+
+    removesqbrackl = str(directions).replace("[", "")
+
+    removesqbrackr = removesqbrackl.replace("]", "")
+
+    removeqs = removesqbrackr.replace("'", "\n")
+
+    removecomma = removeqs.replace(",", "")
+
+    print(removecomma)
+
+    return str(removecomma)
