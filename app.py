01febe41cd83746ce2829e3e6a23969400bcae47
GaryNLOL <garypastel@gmail.com>
2021-10-22 16:42:58 -0500

    Added spanish commands

--- app.py
+++ app.py
@@ -12,6 +12,27 @@ import requests
 from features import *
 ##############################
 
+lang_en = {
+    "define_command": "define",
+    "advice_command": "advice",
+    "joke_command": "joke",
+    "search_command": "search",
+    "crypto_command": "crypto",
+    "directions_command": "directions",
+    "transit_command": "transit"
+}
+
+lang_es = {
+    "define_command": "definir",
+    "advice_command": "consejo",
+    "joke_command": "chiste",
+    "search_command": "buscar",
+    "crypto_command": "criptomoneda",
+    "directions_command": "direcciones",
+    "transit_command": "transito"
+}
+
+lang = lang_en
 
 app = Flask(__name__) 
 
@@ -30,25 +51,25 @@ def sms():
     if len(message_body) > 1:
         user_parameter = message_body[1]
 
-    if key_command == "define":
+    if key_command == lang["define_command"]:
         resp.message(get_definition(user_parameter))
         return str(resp)
-    elif key_command == "advice":
+    elif key_command == lang["advice_command"]:
         resp.message(random_advice())
         return str(resp)
-    elif key_command == "joke":
+    elif key_command == lang["joke_command"]:
         resp.message(give_joke())
         return str(resp)
-    elif key_command == "search":
+    elif key_command == lang["search_command"]:
         resp.message(search_google(user_parameter))
         return str(resp)
-    elif key_command == "crypto":
+    elif key_command == lang["crypto_command"]:
         resp.message(crypto(user_parameter))
         return str(resp)
-    elif key_command == "directions":
+    elif key_command == lang["directions_command"]:
         resp.message(directions(user_parameter, message_body[2]))
         return str(resp)
-    elif key_command == "transit":
+    elif key_command == lang["transit_command"]:
         resp.message(transit(user_parameter, message_body[2]))
         return str(resp)
 
